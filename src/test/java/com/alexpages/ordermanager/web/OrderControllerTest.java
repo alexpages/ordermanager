33462f3e50ad4c651d73ce3833276525e7a5a396
alexpages <alexpagesandreu@gmail.com>
2024-05-02 12:41:42 +0200

    Commit: Modify assertThrows import

--- src/test/java/com/alexpages/ordermanager/web/OrderControllerTest.java
+++ src/test/java/com/alexpages/ordermanager/web/OrderControllerTest.java
@@ -1,6 +1,6 @@
 package com.alexpages.ordermanager.web;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
